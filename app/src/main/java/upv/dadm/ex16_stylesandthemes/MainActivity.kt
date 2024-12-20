96fbf6efaea1b3dc0f94d6919e89c4f2c1bc823c
ddandres <ddandres@disca.upv.es>
2024-12-20 08:12:53 +0100

    Updated to AGP 8.7.3 and gradle version 8.9 Updated to edge-to-edge display

--- app/src/main/java/upv/dadm/ex16_stylesandthemes/MainActivity.kt
+++ app/src/main/java/upv/dadm/ex16_stylesandthemes/MainActivity.kt
@@ -11,12 +11,25 @@
 
 package upv.dadm.ex16_stylesandthemes
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        // Enable edge-to-edge display
+        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+        // Get side margins in pixels
+        val sideMarginPx = resources.getDimensionPixelSize(R.dimen.side_margins)
+        // Prevent the layout from overlapping with system bars in edge-to-edge display
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.top)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(sideMarginPx, systemBars.top, sideMarginPx, systemBars.bottom)
+            insets
+        }
     }
 }
